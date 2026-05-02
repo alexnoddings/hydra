96f41d9d6e5803990dbc2312ba51a96bb82ad6b3
Alex Noddings <alexnoddings@users.noreply.github.com>
2026-05-02 11:37:32 +0100

    Add tests for Config

--- src/config.rs
+++ src/config.rs
@@ -39,3 +39,71 @@ pub async fn load_config() -> Config {
         Err(_) => Config::default(),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TOML: &str = r#"
+    address = "localhost:6789"
+
+    [hosts.localhost]
+    "" = "https://localhost:1000/"
+    "dashboard" = "https://localhost:2000/dev/dashboard"
+
+    [hosts."admin.localhost"]
+    "" = "https://localhost:3000/"
+    "#;
+
+    fn make_test_config() -> Config {
+        toml::from_str::<Config>(TOML).unwrap_or_default()
+    }
+
+    #[test]
+    fn default_config_address_not_empty() {
+        let cfg = Config::default();
+        assert!(!cfg.address.is_empty());
+    }
+
+    #[test]
+    fn default_config_hosts_empty() {
+        let cfg = Config::default();
+        assert!(cfg.hosts.is_empty());
+    }
+
+    #[test]
+    fn get_address_correct() {
+        let cfg = make_test_config();
+        assert_eq!(cfg.address, "localhost:6789");
+    }
+
+    #[test]
+    fn get_target_invalid_invalid_none() {
+        let cfg = make_test_config();
+        assert!(cfg.get_target("invalid", "invalid").is_none());
+    }
+
+    #[test]
+    fn get_target_valid_invalid_none() {
+        let cfg = make_test_config();
+        assert!(cfg.get_target("localhost", "invalid").is_none());
+    }
+
+    #[test]
+    fn get_target_valid_valid_correct() {
+        let cfg = make_test_config();
+        assert_eq!(
+            cfg.get_target("localhost", "dashboard"),
+            Some("https://localhost:2000/dev/dashboard")
+        );
+    }
+
+    #[test]
+    fn get_target_valid_valid_empty_correct() {
+        let cfg = make_test_config();
+        assert_eq!(
+            cfg.get_target("localhost", ""),
+            Some("https://localhost:1000/")
+        );
+    }
+}
