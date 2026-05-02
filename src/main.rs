86c3d67cd5efd3aa1e22f1437bce40ec8ff2b3fe
Alex Noddings <alexnoddings@users.noreply.github.com>
2026-05-02 11:26:59 +0100

    Gracefully handle shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::sync::Arc;
+use tokio::signal;
 
 mod config;
 mod router;
@@ -18,6 +19,32 @@ async fn main() {
     println!("Application started. Press Ctrl+C to shut down.");
 
     axum::serve(listener, router)
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .expect("Error running server");
+
+    println!("Application is shutting down...");
+}
+
+async fn shutdown_signal() {
+    tokio::select! {
+        _ = await_ctrl_c() => {},
+        _ = await_terminate() => {},
+    }
+}
+
+async fn await_ctrl_c() {
+    signal::ctrl_c().await.unwrap()
+}
+
+#[cfg(unix)]
+async fn await_terminate() {
+    let terminate = signal::unix::SignalKind::terminate();
+    signal::unix::signal(terminate).unwrap().recv().await;
+}
+
+#[cfg(not(unix))]
+async fn await_terminate() {
+    // No non-unix implementation
+    std::future::pending::<()>().await;
 }
